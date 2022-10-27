6ebcb0a91b3ddb11f7750938fdf7af69ea7cf43f
Jiří Novák <63288402+TaraJura@users.noreply.github.com>
2022-10-27 23:41:37 +0200

    initial

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+
+let myDropzone = new Dropzone("form#myId", { url: "/instances/file"});
+setTimeout(function () { location.reload(true); }, 15000);
