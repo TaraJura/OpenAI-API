af6ee6165c89ce689d37eb3e88ce63bb1dc636be
jirkanovak <jiri.novak@virtualmaster.com>
2023-02-21 17:05:20 +0100

    finally request to the riot server

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,8 @@
 class StatsController < ApplicationController
   def index
     @request = Typhoeus::Request.new(
-      'https://seznam.cz',
-      method: :post,
+      'https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-f527bb99-1013-4a25-b357-f9e7c7e71889',
+      method: :get,
       body: {},
       params: {},
       headers: {}
