7311e93681227e6cf33c5dbe716fc1e51fea9d2d
jirkanovak <jiri.novak@virtualmaster.com>
2023-02-21 16:35:18 +0100

    improved gemfile, deleted useless files and added root path to the new view with typhoeous request

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,11 @@
 class StatsController < ApplicationController
+  def index
+    @request = Typhoeus::Request.new(
+      'https://seznam.cz',
+      method: :post,
+      body: {},
+      params: {},
+      headers: {}
+    ).run
+  end
 end
